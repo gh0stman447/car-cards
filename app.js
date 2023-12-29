a2cccafcc13fc84894858769ad345a692b83512d
gh0stman447 <o_malygin@bk.ru>
2023-12-30 00:14:35 +0300

    add changes

--- app.js
+++ app.js
@@ -2,11 +2,6 @@ const slides = document.querySelectorAll('.slide');
 
 for (const slide of slides) {
   slide.addEventListener('click', () => {
-    // if (slide.clasList.contains('active')) {
-    //   slide.clasList.remove('active');
-    // } else {
-    //   slide.clasList.add('active');
-    // }
     clearActiveClasses();
     slide.classList.add('active');
   });
